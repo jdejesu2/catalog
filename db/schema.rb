fbe24a94dbccc4b04544ba6e7726cb098737755d
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-13 17:46:06 -0600

    remove department column from db scheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_234020) do
+ActiveRecord::Schema.define(version: 2019_02_13_233752) do
 
   create_table "majors", force: :cascade do |t|
     t.string "name"
-    t.string "department"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
